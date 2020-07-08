7e00bd29cab4d8c56f007da0a5379fd4210a2d24
@maxgfr <maxgolfier@gmail.com>
2020-07-08 18:56:22 +0200

    push

--- App.js
+++ App.js
@@ -12,18 +12,28 @@ import { Synchronize } from './lib/synchronize';
 
 export default function App() {
 
-  _onYoutube = () => {
-
-  }
-
   _onTwitch = () => {
+    var access_token = '';
     Synchronize.getInstance().getTwitchToken()
+      .then((res) => {
+        console.log(res)
+        access_token = res.access_token;
+        return Synchronize.getInstance().getTwitchMetrics(res.access_token)
+      })
+      .then((res) => {
+        console.log(res)
+        return Synchronize.getInstance().getAllTwitch(access_token, res.id)
+      })
       .then((res) => {
         console.log(res)
       })
       .catch((err) => {
         console.log(err)
       })
+  }
+
+  _onYoutube = () => {
+
   }
 
   _onInstagram = () => {
@@ -33,19 +43,19 @@ export default function App() {
   return (
     <View style={styles.container}>
       <StatusBar style="auto" />
-      <TouchableOpacity onPress={_onYoutube} style={[styles.button, { backgroundColor: "#FF0000" }]}>
-        <Text style={styles.text}>Youtube</Text>
-      </TouchableOpacity>
       <TouchableOpacity onPress={_onTwitch} style={[styles.button, { backgroundColor: "#6441a5" }]}>
         <Text style={styles.text}>Twitch</Text>
       </TouchableOpacity>
-      <TouchableOpacity onPress={_onInstagram} >
-      <LinearGradient
+      <TouchableOpacity onPress={_onYoutube} style={[styles.button, { backgroundColor: "#FF0000" }]}>
+        <Text style={styles.text}>Youtube</Text>
+      </TouchableOpacity>
+      <TouchableOpacity onPress={_onInstagram}>
+        <LinearGradient
           colors={['#405DE6', '#5851DB', '#833AB4', '#C13584', '#E1306C', '#FD1D1D', '#F56040', '#F77737', '#FCAF45', '#FFDC80']}
           start={[0.9, 0.1]}
           end={[0.1, 0.9]}
           style={[styles.button, { backgroundColor: "#FD1D1D" }]}>
-        <Text style={styles.text}>Instagram</Text>
+            <Text style={styles.text}>Instagram</Text>
         </LinearGradient>
       </TouchableOpacity>
     </View>
